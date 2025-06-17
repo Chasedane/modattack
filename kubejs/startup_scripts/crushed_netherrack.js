113a8c3cc712f3b9adc96c75f9952e8370a875f3
Bencrafter1208 <b.eckhardt2006@gmail.com>
2025-06-17 21:16:04 +0200

    added WIP Crushed Netherrack and items for making netherrite

---
+++ kubejs/startup_scripts/crushed_netherrack.js
@@ -0,0 +1,11 @@
+StartupEvents.registry('block', event => {
+
+    event.create('crushed_netherrack', 'falling') // Create a new block with ID 'kubejs:crushed_netherrack'
+        .displayName('Crushed Netherrack') // Set the display name of the block
+        .hardness(1) // Set the hardness of the block
+        .resistance(1) // Set the resistance of the block
+        .tagBlock('forge:crushed_netherrack') // Add the block to the 'forge:crushed_netherrack' tag
+        .soundType('gravel')
+        .tagBlock('minecraft:mineable/shovels') 
+        .tagBlock('minecraft:needs_wooden_tool') // Add the block to the 'minecraft:needs_wooden_tool' tag
+})
---
+++ kubejs/startup_scripts/netherrite_items.js
@@ -0,0 +1,36 @@
+StartupEvents.registry('item', event => {
+
+    event.create('netherrack_dust')
+        .displayName('Netherrack Dust')
+        .tag('forge:netherrack_dust')
+
+    event.create('silver_gold_blend')
+        .displayName('Impure Gold Dust')
+        .tag('forge:silver_gold_blend')
+
+    event.create('tiny_debris_nugget')
+        .displayName('Tiny Debris Nugget')
+        .tag('forge:tiny_debris_nugget')
+
+    event.create('tiny_nugget_cast')
+        .displayName('Tiny Nugget Cast')
+        .tag('forge:tiny_nugget_cast')
+
+    event.create('netherrite_chunk')
+        .displayName('Netherrite Chunk')
+        .tag('forge:netherrite_chunk')
+
+    event.create('scrap_sand_mold')
+        .displayName('Scrap Sand Mold')
+        .tag('forge:scrap_sand_mold')
+
+    event.create('dummy_scrap')
+        .displayName('Dummy Scrap')
+        .tag('forge:dummy_scrap')
+
+    event.create('scrap_mold')
+        .displayName('Scrap Mold')
+        .tag('forge:scrap_mold')
+
+    
+})
