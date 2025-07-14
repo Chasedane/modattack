958b1732ab54f705acc37abfc573cf4738f1b343
Bencrafter1208 <b.eckhardt2006@gmail.com>
2025-07-14 16:38:05 +0200

    removed Chisel, added Agricraft and Chipped, deleted Agricraft Resource Plants and Nuggets

---
+++ kubejs/client_scripts/hidden_items.js
@@ -0,0 +1,8 @@
+JEIEvents.hideItems(event => {
+
+  event.hide('agricraft:coal_pebble')
+  event.hide('agricraft:copper_nugget')
+  event.hide('agricraft:diamond_shard')
+  event.hide('agricraft:netherite_sliver')
+  event.hide('agricraft:amathyllis_petal')
+})
